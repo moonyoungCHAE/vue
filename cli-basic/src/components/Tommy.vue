31a7fdcecc643f0ab74da22aa4955440959d473c
moonyoungCHAE <xpf_fl@naver.com>
2020-06-25 19:48:53 +0900

    feat: Single File Component

---
+++ cli-basic/src/components/Tommy.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p> {{ name }}</p>
+        <button @click="updateName">Change Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                name: 'Tommy'
+            }
+        },
+        methods: {
+            updateName() {
+                this.name = 'Tommy Updated'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- cli-basic/src/views/Home.vue
+++ cli-basic/src/views/Home.vue
@@ -1,18 +1,35 @@
+<!--
+  Single File Component 구성
+    1) template : html code
+    2) script : data, method etc
+    3) style : html style
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+    <h1>Tommy Tommy</h1>
+    <p> {{ name }}</p>
+    <tommy></tommy>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    import Tommy from "../components/Tommy";
+  export default {
+      components: {Tommy},
+      comments: {
+          Tommy
+      },
+      data() {
+          return {
+              name: 'Tommy Tommy'
+          }
+      }
   }
-}
 </script>
+
+<!-- scoped 안쓰면 모든 애플리케이션에 적용된다. -->
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
