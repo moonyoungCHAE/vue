e6552b63510e4d5ed9ae02c91dd639a4b6e3227d
moonyoungCHAE <xpf_fl@naver.com>
2020-06-26 12:13:35 +0900

    feat: todo checkbox, 삭제 기능 구현

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,41 +1,53 @@
 <template>
-  <div id="app" class="container">
-    <h1 class="text-center">Todo App</h1>
-    <input type="text" class="w-100 p-2" placeholder="입력하세요." v-model="todoInput" @keyup.enter="addTodo">
-    <hr>
-    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @update="update(todo.index)"></Todo>
-  </div>
+    <div id="app" class="container">
+        <h1 class="text-center">Todo App</h1>
+        <input type="text" class="w-100 p-2" placeholder="입력하세요." v-model="todoInput" @keyup.enter="addTodo">
+        <hr>
+        <Todo v-for="todo in todos" :key="todo.id" :todo="todo"
+              @toggleCheckbox="toggleCheckbox"
+              @deleteCheckbox="deleteCheckbox"></Todo>
+    </div>
 </template>
 
 <script>
-import Todo from "./components/Todo";
+    import Todo from "./components/Todo";
 
-export default {
-  components: {
-    Todo
-  },
-  data() {
-    return {
-      todoInput: '',
-      todos: [
-        { id: 1, text: 'buy a car', checked: false},
-        { id: 2, text: 'buy a pants', checked: true},
-      ]
+    export default {
+        components: {
+            Todo
+        },
+        data() {
+            return {
+                todoInput: '',
+                todos: [
+                    {id: 1, text: 'buy a car', checked: false},
+                    {id: 2, text: 'buy a pants', checked: true},
+                ]
+            }
+        },
+        methods: {
+            // object의 멤버변수 분리되어 들어온다. 함수의 인자이름으로 받아온다.
+            // toogleCheckbox(id, checked) -- 안되는데 ?? id에 object 들어온다.
+            toggleCheckbox(param) {
+                const index = this.todos.findIndex(todo => {
+                    return todo.id === param.id;
+                })
+                this.todos[index].checked = param.checked;
+            },
+            addTodo(e) {
+                this.todos.push({
+                    id: Math.random(),
+                    text: e.target.value,
+                    checked: false
+                })
+                this.todoInput = "";
+            },
+            deleteCheckbox(id) {
+                const index = this.todos.findIndex(todo => {
+                    return todo.id === id;
+                });
+                this.$delete(this.todos, index)
+            }
+        }
     }
-  },
-  methods: {
-    update(todo, index) {
-      console.log(todo.text);
-      console.log(index);
-    },
-    addTodo(e) {
-      this.todos.push({
-        id: Math.random(),
-        text: e.target.value,
-        checked: false
-      })
-      this.todoInput = "";
-    }
-  }
-}
 </script>
--- todo/src/components/Todo.vue
+++ todo/src/components/Todo.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <input type="checkbox" :checked="todo.checked">
-        <span class="m1-3"> {{ todo.text}}</span>
+        {{ todo.checked }}
+        <input type="checkbox" :checked="todo.checked" @change="toggleCheckbox">
+        <span class="m1-3" :style="todo.checked ? 'text-decoration: line-through' : ''">
+        {{ todo.text}}
+        </span>
+        <button @click="deleteBox(todo.id)">DELETE</button>
     </div>
 </template>
 
@@ -11,6 +15,18 @@
             todo: {
                 type: Object,
                 required: true
+            }
+        },
+        methods: {
+            toggleCheckbox(e) {
+                //  보내는 parameter { } 로 묶으면 Object로 전송된다.
+                this.$emit('toggleCheckbox', {
+                    id: this.todo.id,
+                    checked: e.target.checked
+                });
+            },
+            deleteBox(id) {
+                this.$emit('deleteCheckbox', id)
             }
         }
     }
