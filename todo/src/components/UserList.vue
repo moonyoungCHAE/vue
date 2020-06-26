6968b36deefceeaa800fd1aefa272591de32e106
moonyoungCHAE <xpf_fl@naver.com>
2020-06-26 15:32:14 +0900

    feat: vuex map helper

--- todo/src/components/UserList.vue
+++ todo/src/components/UserList.vue
@@ -8,20 +8,31 @@
 </template>
 
 <script>
+    import {mapState, mapActions} from 'vuex'
+
     export default {
         created() {
             this.getUsers()
         },
         // data 로 users 하면 변경 감지가 안된다.
         computed: {
-            users() {
-                return this.$store.state.users
-            }
+            // users() {
+            //     return this.$store.state.users
+            // },
+            // todos() {
+            //     return this.$store.state.todos;
+            // },
+
+            // 위에를 다음과 같이 줄일 수 있다. Map Helper
+            ...mapState([
+                'users', {'name':'todos'}
+            ])
         },
         methods: {
-            getUsers() {
-                this.$store.dispatch('getUsers')
-            }
+            // getUsers() {
+            //     this.$store.dispatch('getUsers')
+            // }
+            ...mapActions(['getUsers'])
         }
 
     }
